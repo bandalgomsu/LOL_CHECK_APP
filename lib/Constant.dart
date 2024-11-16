7d6cf51a8940f2b34c71ddf9a7e53872ba6634c5
bandalgomsu <q63530@naver.com>
2024-11-16 19:53:04 +0900

    feat : 메일 인증 api 추가

--- lib/Constant.dart
+++ lib/Constant.dart
@@ -1,3 +1,4 @@
-const String baseUrl = 'http://10.0.2.2:8080';
+// const String baseUrl = 'http://10.0.2.2:8080';
+const String baseUrl = 'https://www.lolcheck.site';
 const fcmKey =
     "BMeOj5qQzdmvQtsWBUfkA2nU159at9VER-V6iB2MQa270cT19XtRURRXR4SwMiPs0L9k8rMVZHJs2jAxzek4CLA";
--- lib/page/SignUpPage.dart
+++ lib/page/SignUpPage.dart
@@ -15,11 +15,12 @@ class SignUpPage extends StatefulWidget {
 
 class _SignUpPageState extends State<SignUpPage> {
   bool isLoading = false;
+  bool isVerified = false;
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController verificationCodeController =
-  TextEditingController();
+      TextEditingController();
 
   bool isVerificationSent = false;
   bool isResendEnabled = false;
@@ -46,15 +47,69 @@ class _SignUpPageState extends State<SignUpPage> {
   void sendVerificationEmail() {
     // 이메일 인증 메일 발송 로직 추가
     _timer?.cancel();
-    setState(() {
-      isVerificationSent = true;
-      isResendEnabled = true;
-    });
     startCountdown(); // 카운트다운 시작
+    sendSignUpVerifyingMail();
   }
 
-  void verifyEmailCode() {
-    // 이메일 인증 코드 검증 로직 추가
+  Future<void> sendSignUpVerifyingMail() async {
+    final email = emailController.text;
+
+    updateLoading();
+    final url = Uri.parse('$baseUrl/api/v1/auth/signUp/mail');
+
+    try {
+      final response = await http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({'email': email}));
+      if (response.statusCode == 200) {
+        setState(() {
+          isVerificationSent = true;
+          isResendEnabled = true;
+          errorMessage = "";
+        });
+      } else {
+        setState(() {
+          errorMessage = "올바르지 않은 이메일 입니다.";
+        });
+      }
+    } catch (error) {
+      setState(() {
+        errorMessage = '메일 발송 요청 중 오류가 발생했습니다';
+      });
+    }
+
+    updateLoading();
+  }
+
+  Future<void> verifyAuthNumber() async {
+    final email = emailController.text;
+    final authNumber = verificationCodeController.text;
+
+    updateLoading();
+    final url = Uri.parse('$baseUrl/api/v1/auth/signUp/mail/verify');
+
+    try {
+      final response = await http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({'email': email, 'authNumber': authNumber}));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          isVerified = true;
+          errorMessage = "";
+        });
+      } else {
+        setState(() {
+          errorMessage = "잘못된 인증코드 입니다.";
+        });
+      }
+    } catch (error) {
+      setState(() {
+        errorMessage = "잘못된 인증코드 입니다.";
+      });
+    }
+
+    updateLoading();
   }
 
   Future<void> signUp(BuildContext context) async {
@@ -79,7 +134,7 @@ class _SignUpPageState extends State<SignUpPage> {
         await fetchSaveDevice();
         Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) => const MainPage()),
-              (Route<dynamic> route) => false,
+          (Route<dynamic> route) => false,
         );
       } else {
         setState(() {
@@ -99,7 +154,7 @@ class _SignUpPageState extends State<SignUpPage> {
     final url = Uri.parse('$baseUrl/api/v1/devices');
     final accessToken = await TokenManager().getAccessToken();
     final fcmToken =
-    await FirebaseMessaging.instance.getToken(vapidKey: fcmKey);
+        await FirebaseMessaging.instance.getToken(vapidKey: fcmKey);
 
     try {
       final response = await http.post(
@@ -147,82 +202,80 @@ class _SignUpPageState extends State<SignUpPage> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: isLoading
-              ? const CircularProgressIndicator() : Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 8.0),
-                  ElevatedButton(
-                    onPressed: sendVerificationEmail,
-                    child: Text(isResendEnabled ? '재발송' : '이메일 인증 메일 발송'),
-                  ),
-                ],
-              ),
-              if (isVerificationSent) SizedBox(height: 8.0),
-              if (isVerificationSent)
-                Text(
-                  '남은 시간: ${_countdown ~/ 60}:${(_countdown % 60)
-                      .toString()
-                      .padLeft(2, '0')}',
-                  style: TextStyle(color: Colors.red),
-                ),
-              SizedBox(height: 16.0),
-              if (isVerificationSent)
-                Column(
+              ? const CircularProgressIndicator()
+              : Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        ElevatedButton(
+                          onPressed: sendVerificationEmail,
+                          child: Text(isResendEnabled ? '재발송' : '이메일 인증 메일 발송'),
+                        ),
+                      ],
+                    ),
+                    if (isVerificationSent) SizedBox(height: 8.0),
+                    if (isVerificationSent)
+                      Text(
+                        '남은 시간: ${_countdown ~/ 60}:${(_countdown % 60).toString().padLeft(2, '0')}',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    SizedBox(height: 16.0),
+                    if (isVerificationSent)
+                      Column(
+                        children: [
+                          TextField(
+                            controller: verificationCodeController,
+                            decoration: InputDecoration(
+                              labelText: '인증 번호',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(height: 8.0),
+                          ElevatedButton(
+                            onPressed: () => verifyAuthNumber(),
+                            child: Text('이메일 인증'),
+                          ),
+                        ],
+                      ),
+                    SizedBox(height: 16.0),
                     TextField(
-                      controller: verificationCodeController,
+                      controller: passwordController,
+                      obscureText: true,
                       decoration: InputDecoration(
-                        labelText: '인증 번호',
+                        labelText: 'Password',
                         border: OutlineInputBorder(),
                       ),
                     ),
-                    SizedBox(height: 8.0),
+                    SizedBox(height: 16.0),
+                    if (errorMessage.isNotEmpty)
+                      Text(
+                        errorMessage,
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    SizedBox(height: 24.0),
                     ElevatedButton(
-                      onPressed: verifyEmailCode,
-                      child: Text('이메일 인증'),
+                      onPressed: isVerified ? () => signUp(context) : null,
+                      child: Text('회원가입'),
+                      style: ElevatedButton.styleFrom(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      ),
                     ),
                   ],
                 ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              if (errorMessage.isNotEmpty)
-                Text(
-                  errorMessage,
-                  style: TextStyle(color: Colors.red),
-                ),
-              SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: () {
-                  signUp(context);
-                },
-                child: Text('회원가입'),
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
